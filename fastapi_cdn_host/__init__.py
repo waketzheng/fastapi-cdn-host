190fa7eec10aef1093353d7ffbdcd1db0361ca56
Waket Zheng <waketzheng@gmail.com>
2024-08-21 23:27:36 +0800

    fix: import error in executor compiled py pyinstaller (#1)

--- fastapi_cdn_host/__init__.py
+++ fastapi_cdn_host/__init__.py
@@ -1,11 +1,8 @@
-import importlib.metadata
-from pathlib import Path
-
 from .client import AssetUrl, CdnHostEnum, CdnHostItem, monkey_patch_for_docs_ui
 from .utils import today_lock, weekday_lock
 
 patch_docs = monkey_patch = monkey_patch_for_docs_ui
-__version__ = importlib.metadata.version(Path(__file__).parent.name)
+__version__ = "0.7.4"
 __all__ = (
     "__version__",
     "AssetUrl",
