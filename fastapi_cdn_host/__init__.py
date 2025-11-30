f25d77427ed0c84353d10b88560757966cdd568d
Waket Zheng <waketzheng@gmail.com>
2025-12-01 00:22:20 +0800

    ⬆️  Bump version: 0.9.2 → 0.9.3

--- fastapi_cdn_host/__init__.py
+++ fastapi_cdn_host/__init__.py
@@ -9,7 +9,7 @@ from .client import (
 from .utils import today_lock, weekday_lock
 
 monkey_patch = monkey_patch_for_docs_ui
-__version__ = "0.9.2"
+__version__ = "0.9.3"
 __all__ = (
     "__version__",
     "AssetUrl",
